851487f8193a5d028d0644df77bdb573948c1be8
Keyreal <nadobnykh@gmail.com>
2016-11-29 16:25:14 +0100

    Update kina.js

--- js/kina.js
+++ js/kina.js
@@ -1,6 +1,7 @@
 /* $('a[href^=#]').on('click', function(e){ */
 $('.gotostartbutton').on('click', function(e){
   e.preventDefault();
+  console.log('1');
   var href = $(this).attr('href');
   $('html, body').animate({
     scrollTop:$(href).offset().top
