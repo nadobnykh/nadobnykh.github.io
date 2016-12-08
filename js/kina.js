691436e3c8fef1042c632572b1396a71eb9963a3
Keyreal <nadobnykh@gmail.com>
2016-12-08 12:50:12 +0100

    Update kina.js

--- js/kina.js
+++ js/kina.js
@@ -28,6 +28,11 @@ $(window).scroll(function (event) {
     var scrollValue = $(window).scrollTop();
     if(scrollValue > 200){
       $('.mainnav').addClass('stickytop');
+      
+      $( "#mainnav" ).fadeOut( "slow", function() {
+        $('.mainnav').removeClass('stickytop');
+      });
+      
     } else {
       $('.mainnav').removeClass('stickytop');
     }
