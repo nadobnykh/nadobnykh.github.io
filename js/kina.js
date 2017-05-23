8f0eb682ece29eea42d0c460f945d1b7ce2b115b
Nadobnykh <k.nadobnykh@mzentrale.de>
2017-05-23 18:50:55 +0200

    css+js

--- js/kina.js
+++ js/kina.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
 
     // Linkziel in Variable schreiben
     var ziel = $(this).attr("href");
-    var kinaTopOffsetOffset = ($(document).width() > kinaMobileBreakpoint) ? 52 : 55; // DESKTOP : MOBILE
+    var kinaTopOffsetOffset = ($(document).width() > kinaMobileBreakpoint) ? 38 : 55; // DESKTOP : MOBILE
     var newScrollTop = $(ziel).offset().top - kinaTopOffsetOffset;
     var currentScrollTop = window.pageYOffset;
     var scrollDiff = Math.abs(currentScrollTop - newScrollTop);
